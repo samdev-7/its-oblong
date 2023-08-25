c548a113b67e8688d5ef0ba25d2154663afe6cdf
Sam Liu <hello@samliu.dev>
2023-08-25 17:02:21 +0800

    Initial landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- Hero -->
+<div class="py-28 flex flex-col">
+	<p
+		class="mx-auto text-md font-medium text-zinc-700 border-2 border-zinc-300 rounded-full py-1 px-5"
+	>
+		Coming soon! 🚀
+	</p>
+	<div class="flex mx-auto text-6xl font-bold py-6">
+		<h1>
+			Create <span
+				class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent"
+				>Short</span
+			>
+			Links,
+			<span class="relative">
+				<span
+					class="italic bg-gradient-to-br from-teal-500 to-blue-500 bg-clip-text text-transparent"
+				>
+					Fast
+				</span>
+
+				<span
+					class="-z-10 pointer-events-none select-none absolute right-1 italic bg-gradient-to-br from-teal-500/30 to-blue-500/30 bg-clip-text text-transparent"
+					aria-hidden="true"
+				>
+					Fast
+				</span>
+				<span
+					class="-z-20 pointer-events-none select-none absolute right-2 italic bg-gradient-to-br from-teal-50/25 to-blue-500/25 bg-clip-text text-transparent"
+					aria-hidden="true"
+				>
+					Fast
+				</span>
+				<span
+					class="-z-30 pointer-events-none select-none absolute right-3 italic bg-gradient-to-br from-teal-500/10 to-blue-500/10 bg-clip-text text-transparent"
+					aria-hidden="true"
+				>
+					Fast
+				</span>
+			</span>
+		</h1>
+	</div>
+	<p class="text-center text-zinc-800 text-lg py-4">
+		Completely free, popups-less, no sign-ups, and zero ads.<br />Its.obl.ong is the best way to
+		create super short links that can be shared anywhere.
+	</p>
+</div>
