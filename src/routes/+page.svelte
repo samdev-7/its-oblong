f13ffb5ffe97da3e1c42891c2ae72b471a585e0f
Sam Liu <hello@samliu.dev>
2023-08-26 14:19:43 +0800

    Expiry and submit button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 
 			// Prevent flash due to state change on load
 			link_input_e.className = link_input_e.className
-				.replace('border-blue-500', 'border-zinc-300')
+				.replace('border-sky-500', 'border-zinc-400')
 				.replace('ring', '');
 		}
 	});
@@ -35,14 +35,14 @@
 			if (link_input_e) {
 				if (url_valid || link_input.length === 0) {
 					link_input_e.className = link_input_e.className
-						.replace('border-red-500', 'border-zinc-300')
-						.replace('focus:border-red-500', 'focus:border-blue-500')
-						.replace('ring-red-500/50', 'ring-blue-500/50');
+						.replace('border-red-500', 'border-zinc-400')
+						.replace('focus:border-red-500', 'focus:border-sky-500')
+						.replace('ring-red-500/50', 'ring-sky-500/50');
 				} else {
 					link_input_e.className = link_input_e.className
-						.replace('border-zinc-300', 'border-red-500')
-						.replace('focus:border-blue-500', 'focus:border-red-500')
-						.replace('ring-blue-500/50', 'ring-red-500/50');
+						.replace('border-zinc-400', 'border-red-500')
+						.replace('focus:border-sky-500', 'focus:border-red-500')
+						.replace('ring-sky-500/50', 'ring-red-500/50');
 				}
 			}
 		}
@@ -53,74 +53,201 @@
 	<meta http-equiv="refresh" content="0; url=/noscript" />
 </noscript>
 
-<!-- Hero -->
-<div class="py-20 flex flex-col text-center px-8">
-	<p
-		class="bg-white mx-auto text-md font-medium text-zinc-700 border-2 border-zinc-200 rounded-full py-1 px-5"
-	>
-		Coming soon! 🚀
-	</p>
-	<div class="flex mx-auto text-6xl font-bold py-6">
-		<h1>
-			Create <span
-				class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent"
-				>Short</span
-			>
-			Links,
-			<span class="relative">
-				<span
-					class="italic bg-gradient-to-br from-teal-500 to-blue-500 bg-clip-text text-transparent"
+<div class="min-h-screen flex justify-center flex-col">
+	<!-- Hero -->
+	<div class="pb-20 flex flex-col text-center px-8">
+		<p
+			class="bg-white mx-auto text-md font-medium text-zinc-700 border-2 border-zinc-200 rounded-full py-1 px-5"
+		>
+			Coming soon! 🚀
+		</p>
+		<div class="flex mx-auto text-6xl font-bold py-6">
+			<h1>
+				Create <span
+					class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent"
+					>Short</span
 				>
-					Fast
-				</span>
+				Links,
+				<span class="relative">
+					<span
+						class="italic bg-gradient-to-br from-teal-500 to-blue-500 bg-clip-text text-transparent"
+					>
+						Fast
+					</span>
 
-				<span
-					class="-z-10 pointer-events-none select-none absolute right-1 italic bg-gradient-to-br from-teal-500/30 to-blue-500/30 bg-clip-text text-transparent"
-					aria-hidden="true"
-				>
-					Fast
+					<span
+						class="-z-10 pointer-events-none select-none absolute right-1 italic bg-gradient-to-br from-teal-500/30 to-blue-500/30 bg-clip-text text-transparent"
+						aria-hidden="true"
+					>
+						Fast
+					</span>
+					<span
+						class="-z-20 pointer-events-none select-none absolute right-2 italic bg-gradient-to-br from-teal-50/25 to-blue-500/25 bg-clip-text text-transparent"
+						aria-hidden="true"
+					>
+						Fast
+					</span>
+					<span
+						class="-z-30 pointer-events-none select-none absolute right-3 italic bg-gradient-to-br from-teal-500/10 to-blue-500/10 bg-clip-text text-transparent"
+						aria-hidden="true"
+					>
+						Fast
+					</span>
 				</span>
-				<span
-					class="-z-20 pointer-events-none select-none absolute right-2 italic bg-gradient-to-br from-teal-50/25 to-blue-500/25 bg-clip-text text-transparent"
-					aria-hidden="true"
-				>
-					Fast
-				</span>
-				<span
-					class="-z-30 pointer-events-none select-none absolute right-3 italic bg-gradient-to-br from-teal-500/10 to-blue-500/10 bg-clip-text text-transparent"
-					aria-hidden="true"
-				>
-					Fast
-				</span>
-			</span>
-		</h1>
-	</div>
-	<p class="text-center text-zinc-800 text-lg py-4">
-		Completely free, popups-less, no sign-ups, and zero ads.<br />Its.obl.ong is the best way to
-		create super short links that can be shared anywhere.
-	</p>
-</div>
-
-<div class="flex justify-center">
-	<form
-		class="grow flex justify-center max-w-3xl mx-4"
-		novalidate
-		on:submit|preventDefault={(e) => onFormSubmit(e)}
-	>
-		<!-- Link Input -->
-		<div class="w-full">
-			<label for="link-input" class="text-sm text-zinc-500 pl-2">
-				Enter the link you want to shorten
-			</label>
-			<input
-				class="w-full font-medium placeholder-zinc-400 ring border-blue-500 border-2 py-2 px-4 my-0.5 rounded-lg focus:outline-none focus:border-blue-500 focus:ring ring-blue-500/50 motion-safe:transition-colors"
-				type="url"
-				placeholder={link_placeholder}
-				id="link-input"
-				required
-				bind:value={link_input}
-				aria-live="polite"
-			/>
+			</h1>
 		</div>
-	</form>
+		<p class="text-center text-zinc-800 text-lg py-4 font-medium">
+			Completely free, popups-less, no sign-ups, and zero ads.<br />Its.obl.ong is the best way to
+			create super short links that can be shared anywhere.
+		</p>
+	</div>
+
+	<!-- Form -->
+	<div class="flex justify-center">
+		<form
+			class="grow flex flex-col gap-y-4 justify-center max-w-4xl mx-4"
+			novalidate
+			on:submit|preventDefault={(e) => onFormSubmit(e)}
+		>
+			<!-- Link Input Row-->
+			<div class="w-full">
+				<label for="link-input" class="text-sm text-zinc-500 pl-2">
+					What's the link you want to shorten?
+				</label>
+				<input
+					class="w-full font-medium placeholder-zinc-400 ring border-sky-500 border-2 py-2 px-4 my-0.5 rounded-lg focus:outline-none focus:border-sky-500 focus:ring ring-sky-500/50 motion-safe:transition-all"
+					type="url"
+					placeholder={link_placeholder}
+					id="link-input"
+					required
+					bind:value={link_input}
+					aria-live="polite"
+				/>
+			</div>
+
+			<!-- Expiry and Confirmation Row -->
+			<div class="w-full font-medium text-center">
+				<p id="expiryLabel" class="text-sm text-zinc-500 pl-2 text-left font-normal">
+					When should the shortened link expire?
+				</p>
+				<div class="grid grid-cols-8 mt-0.5">
+					<div class="h-full col-span-6 grid grid-cols-6">
+						<div class="flex">
+							<input
+								id="expiry10m"
+								type="radio"
+								name="expiry"
+								value="10m"
+								class="w-0 peer"
+								aria-labelledby="expiry10mLabel"
+								required
+							/>
+							<label
+								id="expiry10mLabel"
+								for="expiry10m"
+								class="grow border-2 border-r border-r-zinc-300 border-zinc-400 py-3 px-3 rounded-l-md peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-r-2 peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								10 min
+							</label>
+						</div>
+						<div class="flex">
+							<input
+								id="expiry1h"
+								type="radio"
+								name="expiry"
+								value="1h"
+								class="w-0 peer"
+								aria-labelledby="expiry1hLabel"
+								checked
+							/>
+							<label
+								id="expiry1hLabel"
+								for="expiry1h"
+								class="grow border-2 border-r border-l border-r-zinc-300 border-l-zinc-300 border-zinc-400 py-3 px-3 peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-r-2 peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								1 hour
+							</label>
+						</div>
+						<div class="flex">
+							<input
+								id="expiry1d"
+								type="radio"
+								name="expiry"
+								value="1d"
+								class="w-0 peer"
+								aria-labelledby="expiry1dLabel"
+							/>
+							<label
+								id="expiry1dLabel"
+								for="expiry1d"
+								class="grow border-2 border-r border-l border-r-zinc-300 border-l-zinc-300 border-zinc-400 py-3 px-3 peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-r-2 peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								1 day
+							</label>
+						</div>
+						<div class="flex">
+							<input
+								id="expiry1M"
+								type="radio"
+								name="expiry"
+								value="1M"
+								class="w-0 peer"
+								aria-labelledby="expiry1MLabel"
+							/>
+							<label
+								id="expiry1MLabel"
+								for="expiry1M"
+								class="grow border-2 border-r border-l border-r-zinc-300 border-l-zinc-300 border-zinc-400 py-3 px-3 peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-r-2 peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								1 month
+							</label>
+						</div>
+						<div class="flex">
+							<input
+								id="expiry1y"
+								type="radio"
+								name="expiry"
+								value="1y"
+								class="w-0 peer"
+								aria-labelledby="expiry1yLabel"
+							/>
+							<label
+								id="expiry1yLabel"
+								for="expiry1y"
+								class="grow border-2 border-r border-l border-r-zinc-300 border-l-zinc-300 border-zinc-400 py-3 px-3 peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-r-2 peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								1 year
+							</label>
+						</div>
+						<div class="flex">
+							<input
+								id="expiryNone"
+								type="radio"
+								name="expiry"
+								value="none"
+								class="w-0 peer"
+								aria-hidden="true"
+								aria-labelledby="expiryNoneLabel"
+							/>
+							<label
+								id="expiryNoneLabel"
+								for="expiryNone"
+								class="grow border-2 border-l border-l-zinc-300 border-zinc-400 py-3 px-3 rounded-r-md peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
+							>
+								No Expiry
+							</label>
+						</div>
+					</div>
+					<div class="col-span-2 ml-12 text-white">
+						<button
+							type="submit"
+							class="w-full h-full rounded-md motion-reduce:bg-sky-500 blue-gradient motion-safe:transition-all motion-safe:hover:shadow-lg motion-safe:hover:scale-105 motion-safe:hover:shadow-sky-500/50 motion-safe:active:scale-100"
+						>
+							Coming Soon!
+						</button>
+					</div>
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
