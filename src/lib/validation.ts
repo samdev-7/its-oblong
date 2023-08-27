8c2a19ea93a143c5797ffd566dcb9de449f3ecd3
Sam Liu <hello@samliu.dev>
2023-08-27 18:41:47 +0800

    Add host blacklist

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,12 +1,27 @@
 import validator from 'validator';
 
+const blocked_hosts = [
+    'its.obl.ong',
+    'its-oblong.vercel.app'
+]
+
 export function validate_url(url: string): boolean {
-    return validator.isURL(
+    const validURL = validator.isURL(
         url,
         {
             protocols: ['http', 'https']
         }
     );
+
+    if (!validURL) {
+        return false;
+    }
+
+    if (blocked_hosts.includes(new URL((url.startsWith('http://') || url.startsWith('https://')) ? url : 'https://' + url).host)) {
+        return false;
+    }
+
+    return true
 }
 
 
