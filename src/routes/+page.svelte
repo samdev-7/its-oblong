ad592b45c555f0b5bce390b9cc02993ed5446b6d
Sam Liu <hello@samliu.dev>
2023-08-28 19:54:48 +0800

    Add Turnstile bot protection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,47 +3,116 @@
 	import { validate_url } from '$lib/validation';
 	import { browser } from '$app/environment';
 	import type { PageData } from './$types';
+	import { Turnstile } from 'svelte-turnstile';
+
+	import { PUBLIC_TURNSTILE_INVISIBLE_KEY, PUBLIC_TURNSTILE_VISIBLE_KEY } from '$env/static/public';
 
 	export let data: PageData;
 
+	let loaded = false;
+
 	let link_placeholder = data.link_placeholder;
 
-	let link_input: string = '';
+	let link_input: string = 'https://google.com';
+	let expiry_input: string = '1h';
+	let turnstile_token: string = '';
+	let turnstile_type: string = '';
+
+	let link_invalid: boolean = false;
+	let turnstile_requires_interaction: boolean = false;
+
+	let loading: boolean = false;
+	let turnstile_validation: boolean = false;
 
 	onMount(() => {
+		loaded = true;
+
 		let link_input_e = document.getElementById('link-input') as HTMLInputElement;
 		if (link_input_e) {
 			link_input_e.focus();
-
-			// Prevent flash due to state change on load
-			link_input_e.className = link_input_e.className
-				.replace('border-sky-500', 'border-zinc-400')
-				.replace('ring', '');
 		}
 	});
 
-	function onFormSubmit(e: SubmitEvent): void {}
+	async function onError(e: string): Promise<void> {
+		console.log(e);
+	}
+
+	async function onFormSubmit(): Promise<void> {
+		if (link_input.length == 0 || !validate_url(link_input)) {
+			return;
+		}
+		if (expiry_input == '') {
+			return;
+		}
+
+		loading = true;
+
+		if (turnstile_token.length == 0) {
+			turnstile_validation = true;
+			return;
+		}
+
+		await shorten(link_input, expiry_input, turnstile_token, turnstile_type);
+
+		turnstile_token = '';
+		turnstile_type = '';
+
+		loading = false;
+	}
+
+	async function shorten(
+		url: string,
+		expiry: string,
+		turnstile_token: string,
+		turnstile_type: string
+	): Promise<void> {
+		const response = await fetch('/api/create', {
+			method: 'POST',
+			body: JSON.stringify({
+				url: url,
+				expiry: expiry,
+				turnstile_token: turnstile_token,
+				turnstile_type: turnstile_type
+			}),
+			headers: {
+				'content-type': 'application/json'
+			}
+		});
+
+		const data = await response.text();
+		console.log(data);
+	}
+
+	async function onTurnstileVisiblePass(e: CustomEvent): Promise<void> {
+		turnstile_requires_interaction = false;
+		const data = e.detail as { token: string };
+		turnstile_token = data.token;
+		turnstile_type = 'visible';
+		turnstile_validation = false;
+		await onFormSubmit();
+	}
+
+	async function onTurnstileInvisiblePass(e: CustomEvent): Promise<void> {
+		const data = e.detail as { token: string };
+		turnstile_token = data.token;
+		turnstile_type = 'invisible';
+		turnstile_validation = false;
+		await onFormSubmit();
+	}
+
+	async function onTurnstileInvisibleFail(e: CustomEvent): Promise<void> {
+		turnstile_requires_interaction = true;
+	}
 
 	$: {
 		link_input = link_input.trim();
 
 		let url_valid = validate_url(link_input);
-		console.log(url_valid);
 
 		if (browser) {
 			let link_input_e = document.getElementById('link-input') as HTMLInputElement;
 			if (link_input_e) {
-				if (url_valid || link_input.length === 0) {
-					link_input_e.className = link_input_e.className
-						.replace('border-red-500', 'border-zinc-400')
-						.replace('focus:border-red-500', 'focus:border-sky-500')
-						.replace('ring-red-500/50', 'ring-sky-500/50');
-				} else {
-					link_input_e.className = link_input_e.className
-						.replace('border-zinc-400', 'border-red-500')
-						.replace('focus:border-sky-500', 'focus:border-red-500')
-						.replace('ring-sky-500/50', 'ring-red-500/50');
-				}
+				link_invalid = !url_valid && link_input.length > 0;
 			}
 		}
 	}
@@ -106,8 +175,9 @@
 	<div class="flex justify-center">
 		<form
 			class="grow flex flex-col gap-y-6 justify-center max-w-4xl mx-4"
+			id="link-form"
 			novalidate
-			on:submit|preventDefault={(e) => onFormSubmit(e)}
+			on:submit|preventDefault={onFormSubmit}
 		>
 			<!-- Link Input Row-->
 			<div class="w-full">
@@ -115,7 +185,11 @@
 					What's the link you want to shorten?
 				</label>
 				<input
-					class="w-full font-medium placeholder-zinc-400 ring border-sky-500 border-2 py-2 px-4 my-0.5 rounded-lg focus:outline-none focus:border-sky-500 focus:ring ring-sky-500/50 motion-safe:transition-all"
+					class="w-full font-medium placeholder-zinc-400
+					{loaded ? (link_invalid ? 'ring-red-500/50' : 'ring-sky-500/50') : 'ring ring-sky-500/50'}
+					{loaded ? (link_invalid ? 'border-red-500' : 'border-zinc-400') : 'border-sky-500'}
+					{link_invalid ? 'focus:border-red-500' : 'focus:border-sky-500'}
+					border-2 py-2 px-4 my-0.5 rounded-lg focus:outline-none focus:ring motion-safe:transition-all"
 					type="url"
 					placeholder={link_placeholder}
 					id="link-input"
@@ -130,8 +204,8 @@
 				<p id="expiryLabel" class="text-sm text-zinc-500 pl-2 text-left font-normal">
 					When should the shortened link expire?
 				</p>
-				<div class="grid grid-cols-8 mt-0.5">
-					<div class="h-full col-span-6 grid grid-cols-6">
+				<div class="flex mt-0.5 items-center">
+					<div class="grow h-full grid grid-cols-6">
 						<div class="flex">
 							<input
 								id="expiry10m"
@@ -140,6 +214,7 @@
 								value="10m"
 								class="w-0 peer"
 								aria-labelledby="expiry10mLabel"
+								bind:group={expiry_input}
 								required
 							/>
 							<label
@@ -158,6 +233,7 @@
 								value="1h"
 								class="w-0 peer"
 								aria-labelledby="expiry1hLabel"
+								bind:group={expiry_input}
 								checked
 							/>
 							<label
@@ -176,6 +252,7 @@
 								value="1d"
 								class="w-0 peer"
 								aria-labelledby="expiry1dLabel"
+								bind:group={expiry_input}
 							/>
 							<label
 								id="expiry1dLabel"
@@ -193,6 +270,7 @@
 								value="1M"
 								class="w-0 peer"
 								aria-labelledby="expiry1MLabel"
+								bind:group={expiry_input}
 							/>
 							<label
 								id="expiry1MLabel"
@@ -210,6 +288,7 @@
 								value="1y"
 								class="w-0 peer"
 								aria-labelledby="expiry1yLabel"
+								bind:group={expiry_input}
 							/>
 							<label
 								id="expiry1yLabel"
@@ -221,31 +300,55 @@
 						</div>
 						<div class="flex">
 							<input
-								id="expiryNone"
+								id="expiry10y"
 								type="radio"
 								name="expiry"
-								value="none"
+								value="10y"
 								class="w-0 peer"
 								aria-hidden="true"
-								aria-labelledby="expiryNoneLabel"
+								aria-labelledby="expiry10yLabel"
+								bind:group={expiry_input}
 							/>
 							<label
-								id="expiryNoneLabel"
-								for="expiryNone"
+								id="expiry10yLabel"
+								for="expiry10y"
 								class="grow border-2 border-l border-l-zinc-300 border-zinc-400 py-3 px-3 bg-white rounded-r-md peer-checked:bg-sky-500 peer-checked:text-white peer-checked:border-sky-600 peer-focus-visible:border-black peer-focus-visible:border-l-2 peer-focus-visible:ring-black"
 							>
 								10 years
 							</label>
 						</div>
 					</div>
-					<div class="col-span-2 ml-12 text-white">
-						<button
-							type="submit"
-							class="w-full h-full rounded-md motion-reduce:bg-sky-500 blue-gradient motion-safe:transition-all motion-safe:hover:shadow-lg motion-safe:hover:scale-105 motion-safe:hover:shadow-sky-500/50 motion-safe:active:scale-100"
-						>
-							Coming Soon!
-						</button>
+					<div
+						class="col-span-2 {turnstile_requires_interaction && loading
+							? 'ml-6'
+							: 'ml-12'} text-white min-w-[11rem]"
+					>
+						{#if turnstile_requires_interaction}
+							<div class="">
+								<Turnstile
+									on:turnstile-callback={onTurnstileVisiblePass}
+									siteKey={PUBLIC_TURNSTILE_VISIBLE_KEY}
+									theme="light"
+								/>
+							</div>
+						{:else}
+							<button
+								type="submit"
+								class="h-full min-w-[11rem] bg-clip-border border-2 border-transparent py-3 rounded-md motion-reduce:enabled:bg-sky-500 blue-gradient motion-safe:disabled:transition-transform motion-safe:enabled:transition-all motion-safe:enabled:hover:shadow-lg motion-safe:enabled:hover:scale-105 motion-safe:enabled:hover:shadow-sky-500/50 motion-safe:enabled:active:scale-100 disabled:bg-slate-400"
+								disabled={loading}
+							>
+								{loading ? 'Loading...' : 'Coming Soon!'}
+							</button>
+						{/if}
 					</div>
+					{#if turnstile_validation}
+						<Turnstile
+							on:turnstile-callback={onTurnstileInvisiblePass}
+							on:turnstile-error={onTurnstileInvisibleFail}
+							siteKey={PUBLIC_TURNSTILE_INVISIBLE_KEY}
+							theme="light"
+						/>
+					{/if}
 				</div>
 			</div>
 		</form>
