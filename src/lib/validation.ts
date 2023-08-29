ef52be224eaa7fc21c8382b688490dc66ce92e83
Sam Liu <hello@samliu.dev>
2023-08-29 17:28:09 +0800

    Add Netlify deploy to block list

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -2,7 +2,8 @@ import validator from 'validator';
 
 const blocked_hosts = [
     'its.obl.ong',
-    'its-oblong.vercel.app'
+    'its-oblong.vercel.app',
+    'its-oblong.netlify.app'
 ]
 
 export function validate_url(url: string): boolean {
