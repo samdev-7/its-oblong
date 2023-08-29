dfb4a8dd30f453e8531abaf19b9a90e26806c4ff
Sam Liu <hello@samliu.dev>
2023-08-29 17:44:27 +0800

    Add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,18 @@
 </script>
 
 <slot />
+
+<footer class="py-4 text-center text-sm text-zinc-700">
+	<p>
+		Its.obl.ong is not approved by or associated with <a
+			href="https://obl.ong"
+			target="_blank"
+			rel="noopener"
+			class="hover:underline">obl.ong</a
+		>.
+	</p>
+	<p>
+		Made with <span class="text-pink-500">❤</span> by
+		<a href="https://samliu.dev" target="_blank" class="hover:underline">Sam Liu</a>.
+	</p>
+</footer>
